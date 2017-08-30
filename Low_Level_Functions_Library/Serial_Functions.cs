3110b91bb67a24260bfaba34b29e94bb3be151b5
HAVERS-SURFACE\sahes <sahesmh@outlook.com>
2017-08-31 07:22:41 +1000

    Added initial QUTMS Windows project

---
+++ Low_Level_Functions_Library/Serial_Functions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.IO.Ports;
+
+namespace Low_Level_Functions_Library
+{
+    public class Serial_Functions
+    {
+        private static string[] portList;
+        private const int connect = 128;
+        private const int disconnect = 1;
+        private static bool ECUconnected = false;
+        private static int portNumberIndex = 0;
+
+        private static int portNumberMax;
+        private static bool errorsEnabled = true;
+        private static bool errorsDisabled = false;
+
+        private static bool[] SerialConnectBool = new bool[2];
+
+        private static ushort addr_chunk = 0;
+        private static long data_chunk = 0;
+        //int responseHeader = 0;
+        private static byte[] incomingString = new byte[60];
+        private static int stringIndex = 0;
+        private static ushort stringMode = 0;
+        private static ushort packetMode = 0;
+        private static List<string[]> TVtable_1;
+
+        private static byte loggingTrue = 0;
+
+        private const int Overview = 1;
+        // Test Change
+
+
+        public static bool IfSerialDetected(bool errorMode) {
+            portList = SerialPort.GetPortNames();
+
+            if (portList.Length > 0) {
+                portNumberMax = portList.Length - 1;
+                portNumberIndex = 0;
+                return true;
+            }
+
+            else {
+                ECUconnected = false;
+                return false;
+            }
+        }
+        public static bool connectionStatus() {
+            return ECUconnected;
+        }
+        public static string returnPortNumber() {
+            return portList[portList.Length - 1];
+        }
+
+        public static bool[] connectSerialLogic(int request) {
+			if (portList.Length > 0) {
+				if (request == connect) {
+					ECUconnected = true;
+					SerialConnectBool[0] = true; //conection status true means connect, false means disconnect
+					SerialConnectBool[1] = true;//true means has ports list
+					return SerialConnectBool;
+				}
+				else {
+					ECUconnected = false;
+					SerialConnectBool[0] = false;
+					SerialConnectBool[1] = true;
+					return SerialConnectBool;
+				}
+			}
+			else if (request == connect) {
+				SerialConnectBool[0] = true;
+				SerialConnectBool[1] = false;
+				return SerialConnectBool;
+			}
+            else{
+                SerialConnectBool[0] = false;
+                SerialConnectBool[1] = false;
+                return SerialConnectBool;
+            }
+        }
+    }
+}
---
+++ QEV1_Windows/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Low_Level_Functions_Library;
+using System.Threading;
+
+namespace QEV1_Windows {
+    public partial class Form1 : Form {
+
+        const int disconect = 1;
+        const int connect = 128;
+        bool[] connectSerialBool = new bool[2] { true, true };
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            ScanSerial(true);
+        }
+
+        private void ScanSerial(bool errorMode) {
+            bool serialDetected = Serial_Functions.IfSerialDetected(errorMode);
+            string portNum = Serial_Functions.returnPortNumber();
+
+            if (serialDetected == true) {
+                ComPortNumber.Text = Serial_Functions.returnPortNumber();
+                serialPort.ReadTimeout = 10000;
+                serialPort.BaudRate = 19200;
+            }
+            else {
+                SerialConnectionStatus.Text = "No Ports";
+                SerialConnectionStatus.BackColor = Color.Yellow;
+                serialConnect(disconect);
+            }
+        }
+
+        private void serialConnect(int request){
+            connectSerialBool = Serial_Functions.connectSerialLogic(request);
+            if (connectSerialBool[0] && connectSerialBool[1]) {
+                serialPort.PortName = ComPortNumber.Text;
+                serialPort.Open();
+                SerialConnectionStatus.Text = "Connected";
+                SerialConnectionStatus.BackColor = System.Drawing.Color.DarkSeaGreen;
+                Thread.Sleep(500);
+            }
+            if (!connectSerialBool[0] && connectSerialBool[1]) {
+                serialPort.Close();
+                SerialConnectionStatus.Text = "Disconnected";
+                SerialConnectionStatus.BackColor = System.Drawing.Color.IndianRed;
+            }
+            if(connectSerialBool[0] && !connectSerialBool[1]) {
+                MessageBox.Show("No Ports Available", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                SerialConnectionStatus.Text = "No Ports";
+            }
+
+            if(!connectSerialBool[0] && !connectSerialBool[1]) {
+                MessageBox.Show("Port Error", "OS Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //statusConnectionStatus.Text = "Port Fault";
+                SerialConnectionStatus.BackColor = Color.Red;
+            }
+        }
+
+        private void SerialConnectionStatus_Click(object sender, EventArgs e) {
+            if (Serial_Functions.connectionStatus()) {
+                serialConnect(disconect);
+            }
+            else {
+                serialConnect(connect);
+            }
+        }
+    }
+}
