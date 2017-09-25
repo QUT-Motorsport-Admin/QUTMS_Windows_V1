e5af218769bc15bd55083b73169b2ae3ff2ea9a7
Joshua Copeland <Joshua-Copeland@users.noreply.github.com>
2017-09-25 14:14:37 +1000

    Added Trip Current Bype getting method to PowerMangement class.

--- QEV1_Windows_Updated/QEV1_Windows_Updated/QEV1_Windows_Updated/PowerManagement.cs
+++ QEV1_Windows_Updated/QEV1_Windows_Updated/QEV1_Windows_Updated/PowerManagement.cs
@@ -9,29 +9,38 @@ namespace QEV1_Windows_Updated
     public class PowerManagement
     {
         // Declare address variables.
-        private int FirstNameAddress;
-        private int FirstConfigAddress;
-        private int NameAddressSeperation;
-        private int ConfigAddressSeperation;
+        private int NameAddress_First;
+        private int ConfigAddress_First;
+        private int TCBAddress_First;
+        private int NameAddress_Seperation;
+        private int ConfigAddress_Seperation;
+        private int TCBAddress_Seperation;
 
         public PowerManagement()
         {
             // Set address variables.
-            FirstNameAddress = 200;
-            FirstConfigAddress = 390;
-            NameAddressSeperation = 16;
-            ConfigAddressSeperation = 1;
-        }
+            NameAddress_First = 200;
+            ConfigAddress_First = 390;
+            TCBAddress_First = 410;
+            NameAddress_Seperation = 16;
+            ConfigAddress_Seperation = 1;
+            TCBAddress_Seperation = 1;
+    }
 
         // Get address variables.
         public int GetChannelNameAddress(int ChannelNumber)
         {
-            return FirstNameAddress + (ChannelNumber - 1) * NameAddressSeperation;
+            return NameAddress_First + (ChannelNumber - 1) * NameAddress_Seperation;
         }
 
         public int GetChannelConfigAddress(int ChannelNumber)
         {
-            return FirstConfigAddress + (ChannelNumber - 1) * ConfigAddressSeperation;
+            return ConfigAddress_First + (ChannelNumber - 1) * ConfigAddress_Seperation;
+        }
+
+        public int GetTCBAddress(int ChannelNumber)
+        {
+            return TCBAddress_First + (ChannelNumber - 1) * TCBAddress_Seperation;
         }
     }
 }
